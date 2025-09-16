ccdf794b8c3f99d07da41fb816f72826151126b1
Yejoon1025 <yejoon1025@gmail.com>
2025-09-15 19:39:07 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Form from "./Form.jsx"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Form />
+  )
+}
+
+export default App
